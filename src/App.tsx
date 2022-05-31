63b0136911b8a1862c7eb6f79ee6b037c3f59923
Alexandra M. Hall <hallstrategygroup@gmail.com>
2022-05-31 13:16:11 -0400

    wishlist-app

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import {
+  createTheme,
+  CssBaseline,
+  List,
+  ListItem,
+  ThemeProvider,
+} from "@mui/material";
+import { FC, useEffect, useState } from "react";
+import { getWishes, Wish } from "./services/wishes";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: `'Noto Sans', sans-serif`,
+    fontWeightRegular: 600,
+  },
+  palette: {
+    mode: "dark",
+    background: {
+      default: "#131924",
+    },
+    primary: {
+      main: "#4d88ff",
+    },
+    secondary: {
+      main: "#a9a9a9",
+    },
+  },
+});
+
+export const App: FC = () => {
+  const [wishes, setWishes] = useState<Wish[]>([]);
+
+  useEffect(() => {
+    getWishes().then(setWishes);
+  }, []);
+  console.log(wishes);
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <List>
+        {wishes.map((wish) => {
+          return <ListItem key={wish.name}>{wish.name}</ListItem>;
+        })}
+      </List>
+    </ThemeProvider>
+  );
+};
